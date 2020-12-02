f1676943a9199c75d7d351c3dfd24851d2059fb5
Chris <chreroax@gmail.com>
2020-12-01 19:32:47 -0600

    Flask app

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
+
+# import the Flask dependency
+from flask import Flask
+
+# Create a new Flask App Instance
+app = Flask(__name__)
+
+# Create our first route! First, we need to define the starting point, also known as the root. To do this,
+#  we'll use the function @app.route('/')
+@app.route('/')
+
+# Create a function called hello_world(). Whenever you make a route in Flask, you put the code you want in that
+#  specific route below @app.route().
+def hello_world():
+    return "Hello World"
